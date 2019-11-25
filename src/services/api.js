5609430adff3abfc68f71db4124258f85829caf2
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-11-24 18:22:09 -0600

    removing old version

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import APIURL from './config'
-import trae from 'trae'
-
-const api = trae.create({
-  baseUrl: APIURL,
-})
-
-export default api
--- src/services/config.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-const HEROKU = process.env.HEROKU || 'https://aaaimx-api.herokuapp.com'
-const LOCAL = process.env.LOCAL || 'http://localhost:3000'
-const DEVELOPMENT = false
-
-module.exports = DEVELOPMENT ? LOCAL : HEROKU
