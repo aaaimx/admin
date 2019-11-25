8bd80ac898cf8502676ce3e2457cee94cc336480
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-11-24 18:32:08 -0600

    deploy to github-pages

--- vue.config.js
+++ vue.config.js
@@ -24,8 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/productivity',
-  outputDir: 'dist',
+  publicPath: '/aaaimx-admin',
+  outputDir: 'docs',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
