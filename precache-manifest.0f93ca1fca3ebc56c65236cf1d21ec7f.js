52bd2da5576a56e1822a10680c42ad079383cf40
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-01 02:59:39 -0600

    Updates

---
+++ precache-manifest.0f93ca1fca3ebc56c65236cf1d21ec7f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a170a7eb7bb257753f1f",
+    "url": "/aaaimx-admin/css/app.cf86db68.css"
+  },
+  {
+    "revision": "efb45585ac3977e753f1",
+    "url": "/aaaimx-admin/css/certificate-form~event-form~profile.006daab7.css"
+  },
+  {
+    "revision": "1347b0555537ed03f0cd02f036f9ebf2",
+    "url": "/aaaimx-admin/index.html"
+  },
+  {
+    "revision": "a170a7eb7bb257753f1f",
+    "url": "/aaaimx-admin/js/app-legacy.71f1f498.js"
+  },
+  {
+    "revision": "9a0b7f5674e4f7b094da",
+    "url": "/aaaimx-admin/js/certificate-form-legacy.bf775a8c.js"
+  },
+  {
+    "revision": "efb45585ac3977e753f1",
+    "url": "/aaaimx-admin/js/certificate-form~event-form~profile-legacy.5f607cc3.js"
+  },
+  {
+    "revision": "c68c2e840b4af03feb83",
+    "url": "/aaaimx-admin/js/chunk-vendors-legacy.5df0c08a.js"
+  },
+  {
+    "revision": "add428199436d4c593cf",
+    "url": "/aaaimx-admin/js/default-legacy.468eb3fc.js"
+  },
+  {
+    "revision": "dbe61165d078590ccb30",
+    "url": "/aaaimx-admin/js/event-form-legacy.c9740bb1.js"
+  },
+  {
+    "revision": "822a9e3976e35351f929",
+    "url": "/aaaimx-admin/js/full-page-legacy.3154fc2a.js"
+  },
+  {
+    "revision": "6ca8e710ea291237064a",
+    "url": "/aaaimx-admin/js/profile-legacy.9493d64e.js"
+  },
+  {
+    "revision": "c0bfb331009ce4a1fc721a86f18ab8a8",
+    "url": "/aaaimx-admin/manifest.json"
+  }
+]);
---
+++ precache-manifest.290adb9e23007d567cb74a2c9d70ed71.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8322db68eb8ad0c07bb3",
+    "url": "/aaaimx-admin/css/app.cf86db68.css"
+  },
+  {
+    "revision": "eb21a584892a253522c0",
+    "url": "/aaaimx-admin/css/certificate-form~profile.006daab7.css"
+  },
+  {
+    "revision": "16ea0dc745ef5a4e286c",
+    "url": "/aaaimx-admin/css/event-form.006daab7.css"
+  },
+  {
+    "revision": "5735ac76596680193ca1953cecf961c2",
+    "url": "/aaaimx-admin/data-sources/clients.json"
+  },
+  {
+    "revision": "46f4eb48300fd5b3d1601ebe15cb7d43",
+    "url": "/aaaimx-admin/data-sources/comments.json"
+  },
+  {
+    "revision": "e1ac77b07129201daa5b1468cff724ab",
+    "url": "/aaaimx-admin/data-sources/stuff-updates.json"
+  },
+  {
+    "revision": "0ce9df127bb8158600422cb4aa23efad",
+    "url": "/aaaimx-admin/data-sources/updates.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/aaaimx-admin/favicon.png"
+  },
+  {
+    "revision": "8a6a63965a0703db4bfe97421e329936",
+    "url": "/aaaimx-admin/index.html"
+  },
+  {
+    "revision": "8322db68eb8ad0c07bb3",
+    "url": "/aaaimx-admin/js/app.42f8aa22.js"
+  },
+  {
+    "revision": "b4bae7774903a935c45f",
+    "url": "/aaaimx-admin/js/certificate-form.e7e21a6d.js"
+  },
+  {
+    "revision": "eb21a584892a253522c0",
+    "url": "/aaaimx-admin/js/certificate-form~profile.321731c0.js"
+  },
+  {
+    "revision": "1249b766e74aced366d0",
+    "url": "/aaaimx-admin/js/chunk-vendors.a2b3203b.js"
+  },
+  {
+    "revision": "acc7d9707acc0be42833",
+    "url": "/aaaimx-admin/js/default.5c901a88.js"
+  },
+  {
+    "revision": "16ea0dc745ef5a4e286c",
+    "url": "/aaaimx-admin/js/event-form.b00494d8.js"
+  },
+  {
+    "revision": "9621bff70edaf5717249",
+    "url": "/aaaimx-admin/js/full-page.a2b3ac83.js"
+  },
+  {
+    "revision": "c996b9937ed12a6980fb",
+    "url": "/aaaimx-admin/js/profile.6eed70e9.js"
+  },
+  {
+    "revision": "c0bfb331009ce4a1fc721a86f18ab8a8",
+    "url": "/aaaimx-admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aaaimx-admin/robots.txt"
+  }
+]);
