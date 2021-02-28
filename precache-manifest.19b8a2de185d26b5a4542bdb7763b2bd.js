0b6fc58635031372fe161acacd6df51892bd5779
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-02-28 14:49:52 -0600

    Updates

---
+++ precache-manifest.19b8a2de185d26b5a4542bdb7763b2bd.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4abaa290e34f33dc5c",
+    "url": "/admin/css/app.cf86db68.css"
+  },
+  {
+    "revision": "867c521a78368cd8c138",
+    "url": "/admin/css/certificate-form.ccf49c88.css"
+  },
+  {
+    "revision": "949f3f948033a79bdec7",
+    "url": "/admin/css/certificate-form~email-form~event-form~events.006daab7.css"
+  },
+  {
+    "revision": "95210caa1350b7f1428e",
+    "url": "/admin/css/certificates.a6fd186d.css"
+  },
+  {
+    "revision": "6aec2c1b3dc400d61a3c",
+    "url": "/admin/css/email-form.06359208.css"
+  },
+  {
+    "revision": "d883b970d56d4ac5d4f6",
+    "url": "/admin/css/event-form~events.24ec03a4.css"
+  },
+  {
+    "revision": "ffc3a91ae9d8d8741797",
+    "url": "/admin/css/full-page.6271c76e.css"
+  },
+  {
+    "revision": "76e0657254b87990edff",
+    "url": "/admin/css/profile.006daab7.css"
+  },
+  {
+    "revision": "deb2a6af79b25a21391ea5952bdb3df8",
+    "url": "/admin/data-sources/clients.json"
+  },
+  {
+    "revision": "46f4eb48300fd5b3d1601ebe15cb7d43",
+    "url": "/admin/data-sources/comments.json"
+  },
+  {
+    "revision": "e1ac77b07129201daa5b1468cff724ab",
+    "url": "/admin/data-sources/stuff-updates.json"
+  },
+  {
+    "revision": "0ce9df127bb8158600422cb4aa23efad",
+    "url": "/admin/data-sources/updates.json"
+  },
+  {
+    "revision": "c673f1bd7106775b5ebee40d17c71750",
+    "url": "/admin/fonts/CooperHewitt.c673f1bd.otf"
+  },
+  {
+    "revision": "79a5925407066e8792e90cd33bddad44",
+    "url": "/admin/img/logo-square.79a59254.png"
+  },
+  {
+    "revision": "3bf148407bd5423d9ccf815414d3bd8d",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "0f4abaa290e34f33dc5c",
+    "url": "/admin/js/app.bd44eb81.js"
+  },
+  {
+    "revision": "867c521a78368cd8c138",
+    "url": "/admin/js/certificate-form.7299b2e2.js"
+  },
+  {
+    "revision": "949f3f948033a79bdec7",
+    "url": "/admin/js/certificate-form~email-form~event-form~events.5f120e4a.js"
+  },
+  {
+    "revision": "95210caa1350b7f1428e",
+    "url": "/admin/js/certificates.8e2105cc.js"
+  },
+  {
+    "revision": "831a16616a8a56ce96d1",
+    "url": "/admin/js/chunk-7010d59d.52e5589f.js"
+  },
+  {
+    "revision": "7eeeee58688f91c53e57",
+    "url": "/admin/js/chunk-vendors.a22cddec.js"
+  },
+  {
+    "revision": "d106c500c2edbb5ac184",
+    "url": "/admin/js/default.29bcf641.js"
+  },
+  {
+    "revision": "6aec2c1b3dc400d61a3c",
+    "url": "/admin/js/email-form.576a2242.js"
+  },
+  {
+    "revision": "f94df8dda9cf9c421412",
+    "url": "/admin/js/event-form.ddfb9eee.js"
+  },
+  {
+    "revision": "d883b970d56d4ac5d4f6",
+    "url": "/admin/js/event-form~events.2b2f8bcf.js"
+  },
+  {
+    "revision": "7e6da105f45a0db3d361",
+    "url": "/admin/js/events.b64793ac.js"
+  },
+  {
+    "revision": "ffc3a91ae9d8d8741797",
+    "url": "/admin/js/full-page.62be37f4.js"
+  },
+  {
+    "revision": "76e0657254b87990edff",
+    "url": "/admin/js/profile.04596556.js"
+  },
+  {
+    "revision": "a039c05e2257c6641d70aca5a7c85769",
+    "url": "/admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin/robots.txt"
+  },
+  {
+    "revision": "1668ad60e2431dfb66190ecb8d96c128",
+    "url": "/admin/service-worker.js"
+  }
+]);
---
+++ precache-manifest.862fa5c77054847884331c1005269358.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35aba8b53ef2800282cc",
+    "url": "/admin/css/app.cf86db68.css"
+  },
+  {
+    "revision": "5aac51f1072650deff5a",
+    "url": "/admin/css/certificate-form.ccf49c88.css"
+  },
+  {
+    "revision": "7ca2f22723db69221274",
+    "url": "/admin/css/certificate-form~email-form~event-form~events.006daab7.css"
+  },
+  {
+    "revision": "d5f1b12996c3389dee7f",
+    "url": "/admin/css/certificates.a6fd186d.css"
+  },
+  {
+    "revision": "48352d346ebe61d155e5",
+    "url": "/admin/css/email-form.06359208.css"
+  },
+  {
+    "revision": "01f4e78ecda2e8ba77a4",
+    "url": "/admin/css/event-form~events.24ec03a4.css"
+  },
+  {
+    "revision": "9a321f45633602ff149d",
+    "url": "/admin/css/full-page.6271c76e.css"
+  },
+  {
+    "revision": "02051ed0eb4528795e1b",
+    "url": "/admin/css/profile.006daab7.css"
+  },
+  {
+    "revision": "c673f1bd7106775b5ebee40d17c71750",
+    "url": "/admin/fonts/CooperHewitt.c673f1bd.otf"
+  },
+  {
+    "revision": "79a5925407066e8792e90cd33bddad44",
+    "url": "/admin/img/logo-square.79a59254.png"
+  },
+  {
+    "revision": "abd2653e96f4664923f07246825cbe10",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "35aba8b53ef2800282cc",
+    "url": "/admin/js/app-legacy.df2d186f.js"
+  },
+  {
+    "revision": "5aac51f1072650deff5a",
+    "url": "/admin/js/certificate-form-legacy.173b1e54.js"
+  },
+  {
+    "revision": "7ca2f22723db69221274",
+    "url": "/admin/js/certificate-form~email-form~event-form~events-legacy.8cc055fa.js"
+  },
+  {
+    "revision": "d5f1b12996c3389dee7f",
+    "url": "/admin/js/certificates-legacy.f10a4bb6.js"
+  },
+  {
+    "revision": "c19a427cb2980d1cbc31",
+    "url": "/admin/js/chunk-6b08b12c-legacy.3adb3cce.js"
+  },
+  {
+    "revision": "975601817bdd106f16b0",
+    "url": "/admin/js/chunk-vendors-legacy.d496642b.js"
+  },
+  {
+    "revision": "3915f1903797bd7f00d7",
+    "url": "/admin/js/default-legacy.d2732c89.js"
+  },
+  {
+    "revision": "48352d346ebe61d155e5",
+    "url": "/admin/js/email-form-legacy.0c82a40d.js"
+  },
+  {
+    "revision": "92a20caceaf1decde49a",
+    "url": "/admin/js/event-form-legacy.f8606768.js"
+  },
+  {
+    "revision": "01f4e78ecda2e8ba77a4",
+    "url": "/admin/js/event-form~events-legacy.b6b4d022.js"
+  },
+  {
+    "revision": "b12f3cd6539ec50f6a34",
+    "url": "/admin/js/events-legacy.8051d063.js"
+  },
+  {
+    "revision": "9a321f45633602ff149d",
+    "url": "/admin/js/full-page-legacy.cf1841dd.js"
+  },
+  {
+    "revision": "02051ed0eb4528795e1b",
+    "url": "/admin/js/profile-legacy.1c526557.js"
+  },
+  {
+    "revision": "a039c05e2257c6641d70aca5a7c85769",
+    "url": "/admin/manifest.json"
+  }
+]);
